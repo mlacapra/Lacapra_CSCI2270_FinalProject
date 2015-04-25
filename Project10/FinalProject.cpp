a552f8f3de9993f86a62bb1c13d8a0415d214c0a
Margherita Lacapra <mala6324@colorado.edu>
2015-04-24 18:02:02 -0600

    Driver File Update
    Adjusted driver file to have separate functions for each unique menu as
    well as adding two new functionalities (reset tree and delete all
    values from tree)

--- Project10/FinalProject.cpp
+++ Project10/FinalProject.cpp
@@ -1,10 +1,8 @@
-//
-//  main.cpp
+//  Driver File
 //  binarySearchTree
 //
 //  Created by Margherita Lacapra on 2/24/15.
 //  Copyright (c) 2015 Margherita Lacapra. All rights reserved.
-//
 
 #include <iostream>
 #include <fstream>
@@ -13,170 +11,184 @@
 using namespace std;
 
 
-void displayMenu()
-{
-    cout << "======Main Menu=====" << endl;
+void mainMenu()
+{   cout << endl << "======Main Menu=====" << endl;
     cout << "1. Tree Functions" << endl;
     cout << "2. Tree Information" << endl;
     cout << "3. Tree Operations" << endl;
-    cout << "4. Print the Tree" << endl;
-    cout << "5. Reset the Tree" << endl;
-    cout << "6. Delete All Entries" << endl;
+    cout << "4. Print Tree" << endl;
+    cout << "5. Reset Tree" << endl;
+    cout << "6. Delete Tree" << endl;
     cout << "7. Quit" << endl;
 }
 
-void functionsMenu()
+void functionsMenu(BinaryTree& tree)
 {
-    cout << "=====Tree Functions=====" << endl;
-    cout << "1. Insert a value" << endl;
-    cout << "2. Lookup a value" << endl;
-    cout << "3. Delete a value" << endl;
-    cout << "4. Main Menu" << endl;
+    string inputLine;
+    int secondChoice = 0;
     
-    getline(cin, inputLine);
-    secondChoice = stoi(inputLine);
-    
-    switch (secondChoice)
+    while (secondChoice != 4)
     {
-        case 1:
-            cout << "Insert value:" << endl;
-            getline(cin, inputLine);
-            tree.insert(stoi(inputLine));
-            break;
-        case 2:
+        cout << endl << "=====Tree Functions=====" << endl;
+        cout << "1. Insert a value" << endl;
+        cout << "2. Lookup a value" << endl;
+        cout << "3. Delete a value" << endl;
+        cout << "4. Main Menu" << endl;
+        
+        getline(cin, inputLine);
+        secondChoice = stoi(inputLine);
+        
+        switch (secondChoice)
         {
-            cout << "Lookup value:" << endl;
-            getline(cin, inputLine);
-            tree.lookup(stoi(inputLine));
-            TreeNode* node = tree.lookup(stoi(inputLine));
-            if (node == NULL)
-                cout << "Value not found." << endl;
-            else
-                cout << "Value found! " << node->key << endl;
+            case 1:
+                cout << "Insert value:" << endl;
+                getline(cin, inputLine);
+                tree.insert(stoi(inputLine));
+                break;
+            case 2:
+            {
+                cout << "Lookup value:" << endl;
+                getline(cin, inputLine);
+                tree.lookup(stoi(inputLine));
+                TreeNode* node = tree.lookup(stoi(inputLine));
+                if (node == NULL)
+                    cout << "Value not found." << endl;
+                else
+                    cout << "Value found! " << node->key << endl;
+            }
+                break;
+            case 3:
+                cout << "Delete value:" << endl;
+                getline(cin, inputLine);
+                tree.deleteNode(stoi(inputLine));
+                break;
+            case 4:
+                break;
         }
-            break;
-        case 3:
-            cout << "Delete value:" << endl;
-            getline(cin, inputLine);
-            tree.deleteNode(stoi(inputLine));
-            break;
-        case 4:
-            secondChoice = 4;
-            break;
     }
 }
 
-void informationMenu()
+void informationMenu(BinaryTree& tree)
 {
-    cout << "=====Tree Information=====" << endl;
-    cout << "1. Tree Size" << endl;
-    cout << "2. Max Depth " << endl;
-    cout << "3. Min Value" << endl;
-    cout << "4. Max Value" << endl;
-    cout << "5. Valid Tree" << endl;
-    cout << "6. Main Menu" << endl;
-    
-    getline(cin, inputLine);
-    secondChoice = stoi(inputLine);
-    
-    switch (secondChoice)
+    string inputLine;
+    int secondChoice = 0;
+
+    while (secondChoice != 6)
     {
-        case 1:
-            cout << "Tree size: " << tree.size() << endl;
-            break;
-        case 2:
-            cout << "Max depth: " << tree.maxDepth() << endl;
-            break;
-        case 3:
-            cout << "Min value: " << tree.minValue() << endl;
-            break;
-        case 4:
-            cout << "Max value: " << tree.maxValue() << endl;
-            break;
-        case 5:
-            if (tree.isBST())
-                cout << "The tree is valid" << endl;
-            else
-                cout << "The tree is not valid." << endl;
-            break;
-        case 6:
-            secondChoice = 6;
-            break;
+    
+        cout << endl <<"=====Tree Information=====" << endl;
+        cout << "1. Tree Size" << endl;
+        cout << "2. Max Depth " << endl;
+        cout << "3. Min Value" << endl;
+        cout << "4. Max Value" << endl;
+        cout << "5. Valid Tree" << endl;
+        cout << "6. Main Menu" << endl;
+        
+        getline(cin, inputLine);
+        secondChoice = stoi(inputLine);
+        
+        switch (secondChoice)
+        {
+            case 1:
+                cout << "Tree size: " << tree.size() << endl;
+                break;
+            case 2:
+                cout << "Max depth: " << tree.maxDepth() << endl;
+                break;
+            case 3:
+                cout << "Min value: " << tree.minValue() << endl;
+                break;
+            case 4:
+                cout << "Max value: " << tree.maxValue() << endl;
+                break;
+            case 5:
+                if (tree.isBST())
+                    cout << "The tree is valid" << endl;
+                else
+                    cout << "The tree is not valid." << endl;
+                break;
+            case 6:
+                break;
+        }
     }
 }
 
-void operationsMenu()
+void operationsMenu(BinaryTree& tree)
 {
-    cout << "=====Tree Operations=====" << endl;
-    cout << "1. Double Tree" << endl;
-    cout << "2. Mirror Tree" << endl;
-    cout << "3. Count Trees" << endl;
-    cout << "4. Find Path Sum" << endl;
-    cout << "5. Main Menu" << endl;
+    string inputLine;
+    int secondChoice = 0;
     
-    getline(cin, inputLine);
-    secondChoice = stoi(inputLine);
-    
-    switch (secondChoice)
+    while (secondChoice != 4)
     {
-        case 1:
-            tree.doubleTree();
-            cout << "Doubled Tree: " << endl;
-            tree.prettyPrint();
-            break;
-        case 2:
-            tree.mirror();
-            cout << "Mirrored Tree: " << endl;
-            tree.prettyPrint();
-            break;
-        case 3:
-            cout << "Enter value:" << endl;
-            getline(cin, inputLine);
-            cout << "Number of possible trees:" << tree.countTrees(stoi(inputLine)) << endl;
-            break;
-        case 4:
-            cout << "Enter value:" << endl;
-            getline(cin, inputLine);
-            if (tree.hasPathSum(stoi(inputLine)))
-                cout << "The answer is yes." << endl;
-            else
-                cout << "The answer is no." << endl;
-            break;
-        case 5:
-            secondChoice = 5;
-            break;
+        cout << endl << "=====Tree Operations=====" << endl;
+        cout << "1. Double Tree" << endl;
+        cout << "2. Mirror Tree" << endl;
+        cout << "3. Find Path Sum" << endl;
+        cout << "4. Main Menu" << endl;
+        
+        getline(cin, inputLine);
+        secondChoice = stoi(inputLine);
+        
+        switch (secondChoice)
+        {
+            case 1:
+                tree.doubleTree();
+                cout << "Doubled Tree: " << endl;
+                tree.prettyPrint();
+                break;
+            case 2:
+                tree.mirror();
+                cout << "Mirrored Tree: " << endl;
+                tree.prettyPrint();
+                break;
+            case 3:
+                cout << "Enter value:" << endl;
+                getline(cin, inputLine);
+                if (tree.hasPathSum(stoi(inputLine)))
+                    cout << "The answer is yes." << endl;
+                else
+                    cout << "The answer is no." << endl;
+                break;
+            case 4:
+                break;
+        }
     }
 }
 
-void printingMenu()
+void printingMenu(BinaryTree& tree)
 {
-    cout << "=====Print the Tree=====" << endl;
-    cout << "1. Postorder Traversal" << endl;
-    cout << "2. Preorder Traversal" << endl;
-    cout << "3. In Order Traversal" << endl;
-    cout << "4. Pretty Print" << endl;
-    cout << "5. Main Menu" << endl;
+    string inputLine;
+    int secondChoice = 0;
     
-    getline(cin, inputLine);
-    secondChoice = stoi(inputLine);
-    
-    switch (secondChoice)
+    while (secondChoice != 5)
     {
-        case 1:
-            tree.printPostOrder();
-            break;
-        case 2:
-            tree.printPreOrder();
-            break;
-        case 3:
-            tree.printInOrder();
-            break;
-        case 4:
-            tree.prettyPrint();
-            break;
-        case 5:
-            secondChoice = 5;
-            break;
+        cout << endl << "=====Print the Tree=====" << endl;
+        cout << "1. Postorder Traversal" << endl;
+        cout << "2. Preorder Traversal" << endl;
+        cout << "3. In Order Traversal" << endl;
+        cout << "4. Pretty Print" << endl;
+        cout << "5. Main Menu" << endl;
+        
+        getline(cin, inputLine);
+        secondChoice = stoi(inputLine);
+        
+        switch (secondChoice)
+        {
+            case 1:
+                tree.printPostOrder();
+                break;
+            case 2:
+                tree.printPreOrder();
+                break;
+            case 3:
+                tree.printInOrder();
+                break;
+            case 4:
+                tree.prettyPrint();
+                break;
+            case 5:
+                break;
+        }
     }
 }
 
@@ -203,7 +215,6 @@ void loadFileData(BinaryTree& tree, const char* filename)
 
 int main(int argc, const char* argv[]) {
     int mainChoice = 0;
-    int secondChoice = 0;
     
     BinaryTree tree;
     
@@ -217,7 +228,7 @@ int main(int argc, const char* argv[]) {
     
     while (mainChoice != 7)
     {
-        displayMenu();
+        mainMenu();
         
         string inputLine;
         getline(cin, inputLine);
@@ -226,42 +237,23 @@ int main(int argc, const char* argv[]) {
         switch (mainChoice)
         {
             case 1:
-                while (secondChoice != 4)
-                {
-                    functionsMenu();
-                }
-                secondChoice = 0;
+                functionsMenu(tree);
                 break;
             case 2:
-                while (secondChoice != 6)
-                {
-                    informationMenu();
-                }
-                secondChoice = 0;
+                informationMenu(tree);
                 break;
             case 3:
-                while (secondChoice != 5)
-                {
-                    operationsMenu();
-                }
-                secondChoice = 0;
+                operationsMenu(tree);
                 break;
-                
             case 4:
-                while (secondChoice != 5)
-                {
-                    printingMenu();
-                }
-                secondChoice = 0;
+                printingMenu(tree);
                 break;
             case 5:
-                //delete all
+                tree.deleteAll();
                 loadFileData(tree, argv[1]);
-                secondChoice = 0;
                 break;
             case 6:
-                //delete all
-                secondChoice = 0;
+                tree.deleteAll();
                 break;
             case 7:
                 cout << "Goodbye!" << endl;
